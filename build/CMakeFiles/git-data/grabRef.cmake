f2dcf548aee44f0fc848c5860f412b3b1ad5f211
goossensbas <goossensbas@gmail.com>
2023-04-20 20:59:31 +0200

    copied mdns component from idf to project

--- build/CMakeFiles/git-data/grabRef.cmake
+++ build/CMakeFiles/git-data/grabRef.cmake
@@ -18,7 +18,7 @@ set(HEAD_HASH)
 file(READ "D:/Documents/elektronica/ESP32/mesh/esp-mdf-test/build/CMakeFiles/git-data/HEAD" HEAD_CONTENTS LIMIT 1024)
 
 string(STRIP "${HEAD_CONTENTS}" HEAD_CONTENTS)
-set(GIT_DIR "C:/Espressif/frameworks/esp-mdf/.git")
+set(GIT_DIR "D:/Documents/elektronica/ESP32/mesh/esp-mdf-test/.git")
 # handle git-worktree
 if(EXISTS "${GIT_DIR}/commondir")
 	file(READ "${GIT_DIR}/commondir" GIT_DIR_NEW LIMIT 1024)
